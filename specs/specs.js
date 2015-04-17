d5ea24ca5d46f19abc2a9d10f9e3720bbf317161
Tanner Heffner <tannerjheffner@gmail.com>
2015-04-17 09:43:09 -0700

    pass all tests for pizza size

--- specs/specs.js
+++ specs/specs.js
@@ -2,10 +2,19 @@ describe("Pizza", function () {
   describe('order', function() {
     it("will return 'small with cheese' for a new pizza", function() {
       var testPizza = Object.create(Pizza);
-
-      console.log(testPizza);
-
       expect(testPizza.order()).to.equal('small with cheese');
+    });
+
+    it("will return 'medium with cheese' for a medium pizza", function() {
+      var testPizza = Object.create(Pizza);
+      testPizza.size = "medium";
+      expect(testPizza.order()).to.equal('medium with cheese');
+    });
+
+    it("will return 'large with cheese' for a large pizza", function() {
+      var testPizza = Object.create(Pizza);
+      testPizza.size = "large";
+      expect(testPizza.order()).to.equal('large with cheese');
     });
 
   });
