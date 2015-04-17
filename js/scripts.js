f0b996704e821295ae914e7797bdb99ca746f6a2
Tanner Heffner <tannerjheffner@gmail.com>
2015-04-17 10:03:15 -0700

    add test for initial price

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,58 @@
 var Pizza = {
   size: "small",
-  toppings: ["cheese"],
+  toppings: "cheese",
 
   order: function() {
-    return this.size + " with " + this.toppings;
+    return this.size + " pizza with " + this.toppings;
+  },
+
+  price: function() {
+      var topCost;
+      var sizeCost;
+
+    if (this.toppings === "cheese") {
+      var topCost = 0
+    } else {
+      var topCost = 1
+  }
+    if (this.size === "small") {
+      var sizeCost = 0
+    }
+    if (this.size === "medium") {
+      var sizeCost = 2
+    }
+    if (this.size === "large") {
+      var sizeCost = 3
+    }
+
+    return 10 + topCost + sizeCost;
   }
 
 };
 
 
-// var order = function(Pizza) {
-//   return size + " " + toppings;
-// };
+$(document).ready(function() {
+
+  $('form#input').submit(function(event) {
+
+    var yourPizza = Object.create(Pizza);
+
+    var inputSize = $('input#size');
+    var inputTopping = $('input#topping');
+
+    yourPizza.size = inputSize;
+    yourPizza.toppings = inputTopping;
+
+    var cost = yourPizza.price();
+
+    $('#order').text(yourPizza.order)
+    $('#cost').text(cost)
 
 
-//
-// $(document).ready(function() {
-//
-//   $('form#input').submit(function(event) {
-//
-//   $('#result').show();
-//   event.preventDefault();
-//
-//   });
-//
-// });
+
+  $('#result').show();
+  event.preventDefault();
+
+  });
+
+});
--- specs/specs.js
+++ specs/specs.js
@@ -2,25 +2,32 @@ describe("Pizza", function () {
   describe('order', function() {
     it("will return 'small with cheese' for a new pizza", function() {
       var testPizza = Object.create(Pizza);
-      expect(testPizza.order()).to.equal('small with cheese');
+      expect(testPizza.order()).to.equal('small pizza with cheese');
     });
 
     it("will return 'medium with cheese' for a medium pizza", function() {
       var testPizza = Object.create(Pizza);
       testPizza.size = "medium";
-      expect(testPizza.order()).to.equal('medium with cheese');
+      expect(testPizza.order()).to.equal('medium pizza with cheese');
     });
 
     it("will return 'large with cheese' for a large pizza", function() {
       var testPizza = Object.create(Pizza);
       testPizza.size = "large";
-      expect(testPizza.order()).to.equal('large with cheese');
+      expect(testPizza.order()).to.equal('large pizza with cheese');
     });
 
     it("will return 'small with pepperoni' for a small pizza w/ pepperoni", function() {
       var testPizza = Object.create(Pizza);
-      testPizza.toppings = ["pepperoni"]
-      expect(testPizza.order()).to.equal('small with pepperoni');
+      testPizza.toppings = "pepperoni";
+      expect(testPizza.order()).to.equal('small pizza with pepperoni');
+    });
+  });
+
+  describe('price', function() {
+    it("will return 10 for a new pizza", function() {
+      var testPizza = Object.create(Pizza);
+      expect(testPizza.price()).to.equal(10);
     });
 
   });
